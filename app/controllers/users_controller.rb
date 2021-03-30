4e4613e2d882aab3cd5e3db2909f7923a855b351
oliviadinicola <oliviadinicola@gmail.com>
2021-03-29 22:38:48 -0400

    hover grey out and like button gradient

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
     def profile
     end
+
+   
+      
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
       has_many :projects, dependent: :destroy
-
+    has_one_attached :photo
 
   end
   
